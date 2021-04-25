6d93021d3008ee76e14e1b88e0a959b9cacee671
chaitradech <kullachandachaitra@gmail.com>
2021-04-25 15:26:21 +0530

    multiply fractions

--- multiply_fractions.go
+++ multiply_fractions.go
@@ -1,7 +1,23 @@
 package main
-
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world.")
+	num1 := 10
+	num2 := 16
+	result1, result2 := reduce(num1, num2)
+	fmt.Println(result1, result2)
+}
+
+func reduce(x, y int) (rest1,rest2 int ) {
+	  gcd := calculateGCD(x, y);
+	  rest1 = x/gcd 
+	  rest2 = y/gcd 
+	    return 
+}
+
+func calculateGCD( x ,y int) int {
+	if (y == 0) {
+		return x
+	}
+	return calculateGCD(y, x % y)
 }
